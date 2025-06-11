637adad0e990e40d682dda02ac4a22a381959e2a
ika-ko <irakli.katamadze.2006@mail.ru>
2025-06-11 14:39:56 +0400

    Add files via upload

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,32 @@
+let calculation = "";
+function calc(value) {
+  switch (value) {
+    case "c":
+      calculation = "";
+      document.getElementById("calcscreen").innerText = " ";
+      console.log("calculation");
+      break;
+    case "=":
+      if (calculation.startsWith("*") || calculation.startsWith("/"))
+        calculation = "";
+      else calculation = eval(calculation);
+      document.getElementById("calcscreen").innerText = calculation;
+      console.log("calculation");
+      break;
+    case "M":
+      calculation = calculation.slice(0, -1);
+      document.getElementById("calcscreen").innerText = calculation;
+      console.log("calculation");
+      break;
 
+    default:
+      if (calculation.length >= 20) return;
+      calculation += value;
+      document.getElementById("calcscreen").innerText = calculation;
+      console.log("calculation");
+  }
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
